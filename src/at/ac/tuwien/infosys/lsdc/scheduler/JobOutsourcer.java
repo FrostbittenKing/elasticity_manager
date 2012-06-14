4a6acd620771b807661adadae59cbafe95b5507d
Grillkohle <Grillkohle@.(none)>
2012-06-14 17:31:04 +0200

    added counting of outsourced jobs

--- src/at/ac/tuwien/infosys/lsdc/scheduler/JobOutsourcer.java
+++ src/at/ac/tuwien/infosys/lsdc/scheduler/JobOutsourcer.java
@@ -7,11 +7,14 @@ public class JobOutsourcer implements IOutsourcedJobCompletionListener{
 	private Double outSourceCostsPerCycle = null;
 	private Double currentCosts = null;
 	private Double totalSumCosts = null;
-	
+	private Integer currentNumberOutsourcedJobs = null;
+	private Integer totalNumberOutsourcedjObs = null;
 	
 	private JobOutsourcer(){		
 		totalSumCosts = 0.0;
 		currentCosts = 0.0;
+		currentNumberOutsourcedJobs = 0;
+		totalNumberOutsourcedjObs = 0;
 	}
 	
 	public synchronized void outSourceJob(OutsourcedJob job){
@@ -20,6 +23,8 @@ public class JobOutsourcer implements IOutsourcedJobCompletionListener{
 		Double jobCosts = job.getExecutionTime() * outSourceCostsPerCycle;
 		currentCosts += jobCosts;
 		totalSumCosts += jobCosts;
+		currentNumberOutsourcedJobs ++;
+		totalNumberOutsourcedjObs ++;
 		new Thread(job).start();
 	}
 	
@@ -37,7 +42,8 @@ public class JobOutsourcer implements IOutsourcedJobCompletionListener{
 	@Override
 	public synchronized void jobCompleted(OutsourcedJob job) {
 		System.out.println("Outsourced job finished: " + job.toString());
-		currentCosts -= job.getExecutionTime() * outSourceCostsPerCycle;		
+		currentCosts -= job.getExecutionTime() * outSourceCostsPerCycle;	
+		currentNumberOutsourcedJobs --;
 	}
 
 	public Double getCurrentCosts() {
@@ -46,5 +52,13 @@ public class JobOutsourcer implements IOutsourcedJobCompletionListener{
 
 	public Double getTotalSumCosts() {
 		return totalSumCosts;
+	}
+
+	public Integer getCurrentNumberOutsourcedJobs() {
+		return currentNumberOutsourcedJobs;
+	}
+
+	public Integer getTotalNumberOutsourcedjObs() {
+		return totalNumberOutsourcedjObs;
 	}
 }
